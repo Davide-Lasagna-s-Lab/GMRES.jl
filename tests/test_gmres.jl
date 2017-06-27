bc44a602bcc9f8172ad02552c570dc618f5ac2b0
Davide Lasagna <davide.lasagna@soton.ac.uk>
2017-06-27 18:59:36 +0100

    improved testing

--- tests/test_gmres.jl
+++ tests/test_gmres.jl
@@ -1,17 +1,63 @@
 using GMRES
 using Base.Test
 
-@testset "gmres         " begin
+@testset "trefthen                               " begin
     # see Trefthen and Bau for this test case
-    m = 2000
+    srand(1)
+    m = 200
     A = 2*eye(m) + 0.5*randn(m, m)/sqrt(m)
     b = randn(m)
         
     # solve
-    x, convres = gmres!(A, b; tol=1e-7, maxiter=10)
+    opts = GMRESOptions(tol=1e-7, maxiter=10)
+    x, convres = gmres!(A, deepcopy(b), opts)
 
-    # check
-    expected = [4.0^(-n) for n = 0:10]
-    @test all(convres.hist .< expected*1.1)
-    @test all(convres.hist .> expected*0.7)
+    # check convergence rate
+    expected = [4.0^(-n) for n = 1:10]
+    @test all(convres.history .< expected*1.1)
+    @test all(convres.history .> expected*0.7)
+
+end
+
+@testset "norm of error                          " begin
+    # see Trefthen and Bau for this test case
+    srand(1)
+    m = 200
+    A = 2*eye(m) + 0.5*randn(m, m)/sqrt(m)
+    x_ex = randn(m)
+    b = A*x_ex
+        
+    # solve with large number of iterations
+    opts = GMRESOptions(tol=1e-7, maxiter=20)
+    x, convres = gmres!(A, deepcopy(b), opts)
+
+    # norm of error
+    @test norm(x - x_ex, 2)/norm(b) < 1e-7
+    @test norm(A*x - b,  2)/norm(b) < 1e-7
+end
+
+@testset "stopping                               " begin
+    # see Trefthen and Bau for this test case
+    srand(1)
+    m = 200
+    A = 2*eye(m) + 0.5*randn(m, m)/sqrt(m)
+    x_ex = randn(m)
+    b = A*x_ex
+        
+    # hit tolerance
+    opts = GMRESOptions(tol=1e-1, maxiter=20)
+    x, convres = gmres!(A, deepcopy(b), opts)
+    @test convres.status == :converged
+    @test norm(A*x - b,  2)/norm(b) < 1e-1
+
+    # hit iterations number
+    for niter = 1:5
+        opts = GMRESOptions(tol=1e-15, maxiter=niter)
+        x, convres = gmres!(A, deepcopy(b), opts)
+        @test convres.status == :maxiterreached
+        @test length(convres.history) == niter
+    end
+
+    # cant set zero iterations
+    @test_throws AssertionError GMRESOptions(tol=1e-15, maxiter=0)
 end
